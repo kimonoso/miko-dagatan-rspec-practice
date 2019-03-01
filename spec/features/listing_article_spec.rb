b44a20e3462b22918e27c715174360722ca6d667
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2019-03-01 19:16:23 +0800

    Show new article button to signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -12,7 +12,7 @@
                                 user: @john)
    end
 
-   scenario 'A user lists all articles' do
+   scenario 'With articles created and users not signed in' do
      visit '/'
 
      expect(page).to have_content(@article1.title)
@@ -22,6 +22,22 @@
      expect(page).to have_content(@article2.title)
      expect(page).to have_content(@article2.body)
      expect(page).to have_link(@article2.title)
+     expect(page).not_to have_link('New Article')
+
+   end
+
+   scenario 'With articles created and user signed in' do
+     login_at(@john)
+     visit '/'
+
+     expect(page).to have_content(@article1.title)
+     expect(page).to have_content(@article1.body)
+     expect(page).to have_link(@article1.title)
+
+     expect(page).to have_content(@article2.title)
+     expect(page).to have_content(@article2.body)
+     expect(page).to have_link(@article2.title)
+     expect(page).not_to have_link('New Article')
    end
 
    scenario 'A user has no articles' do
