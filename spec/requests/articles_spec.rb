53cf2f4937207312a386e5eb9f378653c97a22d2
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2019-03-01 13:26:33 +0800

    fixed articles request spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Articles', type: :request do
 
   describe 'GET /articles/:id' do
     context 'with existing article' do
-      before { get "articles/#{article.id}" }
+      before { get "/articles/#{@article.id}" }
 
       it 'handles existing article' do
         expect(response.status).to eq 200
