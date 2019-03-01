99b5805c587549d6421fc5b595ff427725b888c3
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2019-03-01 19:51:52 +0800

    added restriction on controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,15 +26,27 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    unless user_signed_in?
+      flash[:alert] = 'You need to sign in or sign up before continuing'
+    end
+    unless @article.user == current_user
+      flash[:alert] = 'You can only edit your own article'
+      redirect_to root_path
+    end
   end
 
   def update
-    if @article.update(article_params)
-      flash[:success] = 'Article has been updated'
-      redirect_to @article
+    unless @article.user == current_user
+      flash[:alert] = 'You can only edit your own article'
+      redirect_to root_path
     else
-      flash.now[:danger] = 'Article has not been updated'
-      render :edit
+      if @article.update(article_params)
+        flash[:success] = 'Article has been updated'
+        redirect_to @article
+      else
+        flash.now[:danger] = 'Article has not been updated'
+        render :edit
+      end
     end
   end
 
